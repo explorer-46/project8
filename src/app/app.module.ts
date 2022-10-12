c308e26ab4e248dc494fc5948bcc3e48d893514d
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-12 17:21:13 +0530

    sv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { QoutesComponent } from './qoutes/qoutes.component';
+
+
+const link:Routes=[
+  {path:"",component:QoutesComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QoutesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(link)
   ],
   providers: [],
   bootstrap: [AppComponent]
